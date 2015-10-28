bdb887c555b1d8b26ad45016110d704d6f7ab4dc
yuexy <yuexiaoyu002@gmail.com>
2015-10-28 16:32:21 +0800

    完成拍照功能

--- app/src/main/java/com/helloworld/logorecognize/MainActivity.java
+++ app/src/main/java/com/helloworld/logorecognize/MainActivity.java
@@ -1,10 +1,15 @@
 package com.helloworld.logorecognize;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 import com.helloworld.logorecognize.baidu.BaiduUtil;
 import com.helloworld.logorecognize.tietuku.TieTuKuUtils;
@@ -13,13 +18,19 @@ import com.helloworld.logorecognize.tietuku.token.Token;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Date;
 
 
 public class MainActivity extends ActionBarActivity
 {
+	private static final String PATH = Environment.getExternalStorageDirectory().getAbsolutePath() + "/logo";
+
+	private ImageView imageView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -32,6 +43,27 @@ public class MainActivity extends ActionBarActivity
 
 	private void init()
 	{
+		File root = new File(PATH);
+		if (!root.exists())
+			root.mkdirs();
+
+		imageView = (ImageView) findViewById(R.id.testImg);
+
+
+		Intent it = new Intent("android.media.action.IMAGE_CAPTURE");
+		it.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File(PATH + "/hello.png")));
+
+		startActivityForResult(it, 1);
+	}
+
+	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+		if (requestCode != 1)
+			return;
+
+		if (resultCode != RESULT_OK)
+			return;
+
 		new Thread()
 		{
 			public void run()
@@ -39,7 +71,7 @@ public class MainActivity extends ActionBarActivity
 				String token = Token.createToken(new Date().getTime() + 3600, 1145559, "{\"height\":\"h\",\"width\":\"w\",\"s_url\":\"url\"}");
 				System.out.println("TOKEN=> " + token);
 
-				File f = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/JAVA.jpg");
+				File f = new File(PATH + "/hello.png");
 				String res = TieTuKuUtils.doUpload(f, token);
 
 				System.out.println("resule=> " + res);
@@ -67,5 +99,6 @@ public class MainActivity extends ActionBarActivity
 
 			}
 		}.start();
+
 	}
 }
